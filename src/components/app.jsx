5bc99b1ff897d7840f729a6a1dd273bb5f8b945a
Tom Hammersley <thomas.j.hammersley@gmail.com>
2020-09-21 11:09:18 +0100

    lists daily forecast components

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 import LocationDetails from "./location-details";
+import ForecastSummaries from "./forecast-summaries";
 
 const App = (props) => (
-  <LocationDetails
-    city={props.location.city}
-    country={props.location.country}
-  />
+  <div className="forecast">
+    <LocationDetails
+      city={props.location.city}
+      country={props.location.country}
+    />
+
+    <ForecastSummaries forecasts={props.forecasts} />
+  </div>
 );
 
 App.propTypes = {
@@ -14,6 +19,7 @@ App.propTypes = {
     city: PropTypes.string,
     country: PropTypes.string,
   }).isRequired,
+  forecasts: PropTypes.array.isRequired,
 };
 
 export default App;
