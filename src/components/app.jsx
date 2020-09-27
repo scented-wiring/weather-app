9abfbfc848dbf50c7fba4bb355e92e520705797f
Tom Hammersley <thomas.j.hammersley@gmail.com>
2020-09-27 15:31:50 +0100

    app can display city forecasts by search queryy

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import LocationDetails from "./location-details";
 import ForecastSummaries from "./forecast-summaries";
 import ForecastDetails from "./forecast-details";
+import SearchForm from "./search-form";
 import axios from "axios";
 
 import "../styles/app.css";
@@ -30,9 +31,24 @@ const App = (props) => {
     setSelectedDate(date);
   };
 
+  const searchForCity = (city) => {
+    axios
+      .get("https://mcr-codes-weather.herokuapp.com/forecast?city=" + city)
+      .then((res) => {
+        setForecasts(res.data.forecasts);
+        setLocation(res.data.location);
+      });
+  };
+
+  const handleKeyPress = (e) => {
+    searchForCity(e.target.value);
+  };
+
   return (
     <div className="forecast">
       <LocationDetails city={location.city} country={location.country} />
+
+      <SearchForm searchCity={searchForCity} handleKeyPress={handleKeyPress} />
 
       <ForecastSummaries
         forecasts={forecasts}
---
+++ src/components/search-form.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const SearchForm = (props) => {
+  const [searchText, setSearchText] = useState("");
+
+  const handleInputChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  return (
+    <div className="search-form">
+      <input type="text" onChange={handleInputChange} value={searchText} />
+      <button type="button" onClick={() => props.searchCity(searchText)}>
+        Search
+      </button>
+    </div>
+  );
+};
+
+export default SearchForm;
