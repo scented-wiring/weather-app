d48f5dd54a0de7e34992601465fb31ef6bdc8c15
Tom Hammersley <thomas.j.hammersley@gmail.com>
2020-09-28 17:00:45 +0100

    added additional styling

--- src/components/forecast-details.js
+++ src/components/forecast-details.js
@@ -4,12 +4,14 @@ import moment from "moment";
 const ForecastDetails = (props) => {
   return (
     <div className="forecast-details">
-      <div>{moment(props.forecast.date).format("ddd Do MMM")}</div>
+      <div className="forecast-details-date">
+        {moment(props.forecast.date).format("ddd Do MMM")}
+      </div>
       <div>Max temperature: {props.forecast.temperature.max}°c</div>
       <div>Min temperature: {props.forecast.temperature.min}°c</div>
       <div>Humidity: {props.forecast.humidity}</div>
       <div>Wind speed: {props.forecast.wind.speed}</div>
-      <div>Wind direction: {props.forecast.wind.direction}</div>
+      <div>Wind direction: {props.forecast.wind.direction.toUpperCase()}</div>
     </div>
   );
 };
